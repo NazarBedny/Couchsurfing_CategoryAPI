bc48f6dc3d807443f9a08055d1021bd4bb080af7
NazarBedny <nazarbedny@gmail.com>
2023-02-23 19:03:17 +0200

    Add project files.

---
+++ Couchsurfing_CategoryAPI/Controllers/CategoryController.cs
@@ -0,0 +1,152 @@
+﻿using DAL.Models;
+using DAL.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Couchsurfing_CategoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private IUnitOfWork _ADOuow;
+        public CategoryController(ILogger<CategoryController> logger,
+            IUnitOfWork ado_unitofwork)
+        {
+            _logger = logger;
+            _ADOuow = ado_unitofwork;
+        }
+
+        //GET: api/events
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetAllEventsAsync()
+        {
+            try
+            {
+                var results = await _ADOuow._categoryRepository.GetAllAsync();
+                _ADOuow.Commit();
+                _logger.LogInformation($"Отримали всі івенти з бази даних!");
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //GET: api/events/Id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetByIdAsync(int id)
+        {
+            try
+            {
+                var result = await _ADOuow._categoryRepository.GetAsync(id);
+                _ADOuow.Commit();
+                if (result == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInformation($"Отримали івент з бази даних!");
+                    return Ok(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //POST: api/events
+        [HttpPost]
+        public async Task<ActionResult> PostEventAsync([FromBody] Category evnt)
+        {
+            try
+            {
+                if (evnt == null)
+                {
+                    _logger.LogInformation($"Ми отримали пустий json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogInformation($"Ми отримали некоректний json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є некоректним");
+                }
+                var created_id = await _ADOuow._categoryRepository.AddAsync(evnt);
+                _ADOuow.Commit();
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі PostEventAsync - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //POST: api/events/id
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateEventAsync(int id, [FromBody] Category evnt)
+        {
+            try
+            {
+                if (evnt == null)
+                {
+                    _logger.LogInformation($"Ми отримали пустий json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogInformation($"Ми отримали некоректний json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є некоректним");
+                }
+
+                var event_entity = await _ADOuow._categoryRepository.GetAsync(id);
+                if (event_entity == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+
+                await _ADOuow._categoryRepository.ReplaceAsync(evnt);
+                _ADOuow.Commit();
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі PostEventAsync - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //GET: api/events/Id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteByIdAsync(int id)
+        {
+            try
+            {
+                var event_entity = await _ADOuow._categoryRepository.GetAsync(id);
+                if (event_entity == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+
+                await _ADOuow._categoryRepository.DeleteAsync(id);
+                _ADOuow.Commit();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+    }
+}
---
+++ Couchsurfing_CategoryAPI/Program.cs
@@ -0,0 +1,41 @@
+using DAL.Repositories.Contracts;
+using DAL.Repositories;
+using System.Data;
+using System.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped((s) => new SqlConnection(builder.Configuration.GetConnectionString("MSSQLConnection")));
+builder.Services.AddScoped<IDbTransaction>(s =>
+{
+    SqlConnection conn = s.GetRequiredService<SqlConnection>();
+    conn.Open();
+    return conn.BeginTransaction();
+});
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
